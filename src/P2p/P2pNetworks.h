0ad9d45d62adec8e9f67653646328bcbc8f1dc7f
root <rpghill@gmail.com>
2017-09-12 16:39:54 +0200

    First uNote commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2017 MicroNote Coin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x77, 0x92, 0x9a, 0x5b, 0x44, 0x3f, 0x7e, 0x4a, 0x24, 0x15, 0x01, 0x22, 0x37, 0x1a, 0x81, 0x99 } };
+}
